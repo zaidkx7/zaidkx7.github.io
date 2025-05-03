ea3256faba688d853a6598ee13edd9405d5ce7e7
Muhammad Zaid <zaidkx37@gmail.com>
2025-05-03 18:14:33 +0500

    fixed base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/zaidkx7.github.io/",
+  base: "/",
   server: {
     host: "::",
     port: 8080,
