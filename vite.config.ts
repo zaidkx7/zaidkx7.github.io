b4909dda0763c12609c3bbd6ebb217061b9fefaa
Muhammad Zaid <zaidkx37@gmail.com>
2025-05-19 15:34:47 +0500

    cleaned sitemap code, removed unecessary code from vite config file

--- vite.config.ts
+++ vite.config.ts
@@ -15,11 +15,6 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  build: {
-    rollupOptions: {
-      input: "/index.html",
-    },
-  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
